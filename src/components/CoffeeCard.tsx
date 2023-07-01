7432f722d06c9966e0d37bc0bdc1d1e947b9023c
Diwan <diwanmuhamad37@gmail.com>
2023-07-01 16:02:54 +0700

    finish coffee card

--- src/components/CoffeeCard.tsx
+++ src/components/CoffeeCard.tsx
@@ -1,6 +1,6 @@
-import {View, Text, Image} from 'react-native'
+import {View, Text, Image, TouchableOpacity} from 'react-native'
 import { themeColors } from '../theme'
-import { StarIcon } from 'react-native-heroicons/solid'
+import { PlusIcon, StarIcon } from 'react-native-heroicons/solid'
 
 const CoffeeCard = ({item}: any) => {
     return (
@@ -32,12 +32,12 @@ const CoffeeCard = ({item}: any) => {
             style={{
                 borderRadius: 40,
                 backgroundColor: themeColors.bgDark,
-                height: 300,
+                height: 330,
                 width: 250
             }}
             >
                 
-                <View className='px-5 mt-[150px] space-y-3'>
+                <View className='px-5 mt-[120px] space-y-3'>
                     <Text className='text-3xl text-white font-semibold'>
                         {item.name}
                     </Text>
@@ -46,6 +46,25 @@ const CoffeeCard = ({item}: any) => {
                     >
                         <StarIcon size="15" color="white"/>
                         <Text className='text-base font-semibold text-white'>{item.stars}</Text>
+                    </View>
+                    <View className='flex-row space-x-1 mb-3'>
+                        <Text className='text-base text-white font-semibold opacity-60'>
+                            Volume
+                        </Text>
+                        <Text className='text-base text-white font-semibold'>
+                            {item.volume}
+                        </Text>
+                    </View>
+                    <View 
+                    className='flex-row justify-between items-center'>
+                        <Text className='text-white font-bold text-lg'>$ {item.price}</Text>
+                        <TouchableOpacity
+                        style={{
+                            elevation: 20
+                        }}
+                        className='p-4 bg-white rounded-full'>
+                            <PlusIcon size="25" strokeWidth={2} color={themeColors.bgDark} />
+                        </TouchableOpacity>
                     </View>
                 </View>
             </View>
